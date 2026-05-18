0039c915b96b7d53f0ba7e48fd8dfa545cffa99e
kamal khalaf <kamalkhalaf90@gmail.com>
2026-05-18 18:19:47 +0300

    add day 2

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
